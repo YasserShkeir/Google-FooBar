616d1954f3058b8873a505aa96ab8fd9964046fd
YasserShkeir <59066988+YasserShkeir@users.noreply.github.com>
2022-01-04 07:20:29 +0200

    Create MinionTaskScheduling.java

---
+++ MinionTaskScheduling.java
@@ -0,0 +1,41 @@
+package foobar.minion.task.scheduling;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class FooBarMinionTaskScheduling {
+    public static int occurences(int[] list, int n){
+        int count = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] == n) {
+                count++;
+            }
+        }
+        return count;
+    }
+        
+    public static int[] answer(int[] list, int n) {
+        LinkedList<Integer> tst = new LinkedList<>();
+        
+        for (int i = 0; i < list.length; i++) {
+            if (occurences(list, list[i]) <= n) {
+                tst.add(list[i]);
+            }
+        }
+        Object[] arr = tst.toArray();
+        
+        int[] res = new int[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = (Integer)arr[i];
+        }
+        
+        return res;
+    }    
+
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3}; int n = 6;
+        System.out.println(Arrays.toString(answer(list,n)));
+    }
+   
+}
